ed125f266bd00404968e91e9a880b3b0426201cc
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-23 17:01:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,6 @@
 class Dog
   # code goes here
-  attr_accessor :mood, :owner
-  attr_writer :name
+  attr_accessor :name
   
-  @@all = []
-
-  def initialize(name, owner)
-    @name = name
-    @owner = owner
-    @mood = "nervous"
-    @@all << self
-    @owner.dogs << self
-  end
   
-  def self.all
-    @@all
-  end
 end
