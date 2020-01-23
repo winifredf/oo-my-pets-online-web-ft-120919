2a6d867c0c2fd7846ea41af3da3001f53d85a42d
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-23 16:53:31 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,15 +1,13 @@
 class Cat
   attr_reader :name
-  attr_accessor :mood, :owner
+  attr_accessor :owner
   
   @@all = []
   
   def initialize(name, owner)
     @name = name
     @owner = owner
-    @mood = "nervous"
-    @all << self
-    owner.cats << self
+    @@all << self
   end
   
   def self.all
