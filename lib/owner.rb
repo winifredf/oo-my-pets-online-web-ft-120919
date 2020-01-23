bfc62199a885d7f807478e76d10fd9140c6d2bd6
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-23 15:57:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,6 @@
-require_relative def say_species
+require_relative '../config/environment'
+
+  def say_species
     "I am a #{@species}."
   end
   
@@ -83,27 +85,3 @@ require_relative def say_species
     @@owners.length
   end
 end
-class Owner
-  # code goes here
-
-  attr_reader :species, :name
-
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @species = "human"
-    @@all << self
-  end
-
-  def cats
-    Cat.all.select {|cat| cat.owner == self}
-  end
-    
-
-  def dogs
-    Dog.all.select {|dog| dog.owner == self|
-  end
-
-
-  
