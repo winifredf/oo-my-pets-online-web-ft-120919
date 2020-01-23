0d8d9eeea63b547c6a7ab44066d29d6c1a6555ec
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-23 16:36:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,14 @@
 class Owner
   
   attr_reader :name
+  attr_reader :species
   
   def initialize(name)
     @name = name
     
+  end
+  
+  def initialize(species)
+    @species = species
   end
 end
